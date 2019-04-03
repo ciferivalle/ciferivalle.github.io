c94fbfbc2077f5c007df108dae80f369c013ef92
ciferivalle <nataprawiraf@gmail.com>
2019-04-03 17:47:28 +0700

    Updates

---
+++ precache-manifest.ba2b920d513c481d33f665e256c7aab6.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = [
+  {
+    "revision": "3354ccb4057f5ff071542e1e9b416248",
+    "url": "/static/media/logo.3354ccb4.png"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  },
+  {
+    "revision": "a515f3c61f8b4a906977",
+    "url": "/static/js/main.a5b8d2bd.chunk.js"
+  },
+  {
+    "revision": "6e041db76e7a5f978ee8",
+    "url": "/static/js/2.94c272ad.chunk.js"
+  },
+  {
+    "revision": "a515f3c61f8b4a906977",
+    "url": "/static/css/main.5fb84a83.chunk.css"
+  },
+  {
+    "revision": "6e041db76e7a5f978ee8",
+    "url": "/static/css/2.55aa2937.chunk.css"
+  },
+  {
+    "revision": "c2af725be03879c2d8aca5f37b1d271e",
+    "url": "/index.html"
+  }
+];
