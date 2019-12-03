be50653f61c7c29cb9719deef1e1565b7708d173
nataprawiraf <nataprawiraf@gmail.com>
2019-12-04 06:50:18 +0700

    edit footer

--- src/Components/Footer.js
+++ src/Components/Footer.js
@@ -7,13 +7,19 @@ class Footer extends Component {
     render() {
         return (
             <section className="text-center bg-grey py-5">
-                <p class="love white"> &lt;&gt; with ♥ by <a className="" href="http://github.com/ciferivalle">ciferivalle</a></p>
+                <p class="love white"> &lt;&gt; with ♥ by <a className="" href="http://github.com/natprawiraf">natprawiraf</a></p>
 
                 <p className="futura white lead">
                     reach me on:
                 </p>
                 <h2 className="futura white">
-                    <a className="link link2" href="http://github.com/ciferivalle">GitHub</a>, <a className="link" href="http://gitlab.com/ciferivalle">GitLab</a>, <a className="link" href="http://linkedin.com/in/nataprawiraf">LinkedIn</a>
+                    <a className="link link2" href="http://github.com/nataprawiraf">GitHub</a>, <a className="link" href="http://gitlab.com/natprawiraf">GitLab</a>, <a className="link" href="http://linkedin.com/in/nataprawiraf">LinkedIn</a>
+                </h2>
+                <p className="futura white lead">
+                    fiction works:
+                </p>
+                <h2 className="futura white">
+                    <a className="link link2" href="https://byteofscript.wordpress.com/">Poetry</a>
                 </h2>
             </section>
                
